68273517cf8b9781f0bd33436f34165d2c01d8bc
Saim <saimkhanwah@gmail.com>
2025-12-23 23:55:20 +0500

    Remove duplicate ArrowLeft import

--- saimcpp-react/src/pages/UserProfile.jsx
+++ saimcpp-react/src/pages/UserProfile.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import { ArrowLeft } from 'lucide-react'
 import { useParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
